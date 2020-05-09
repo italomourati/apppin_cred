e5d4cb38a39d0110d158458d8d8ed65da0deed4d
Lojas Pintos <danielmatos@lojaspintos.com.br>
2020-05-08 21:34:32 -0300

    page/ restantes -2

--- app/controllers/page/privacy_controller.rb
+++ app/controllers/page/privacy_controller.rb
@@ -2,5 +2,6 @@ class Page::PrivacyController < PageController
   def index
     @users_office_privacies = UsersOffice::Privacy.all
     @users_office_terms = UsersOffice::Term.all
+    @users_office_footers = UsersOffice::Footer.all
   end
 end

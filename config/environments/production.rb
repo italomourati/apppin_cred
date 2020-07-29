8dd4055b4e0696daaa16ff461b2e3424ad219bb3
Lojas Pintos <danielmatos@lojaspintos.com.br>
2020-07-29 12:58:36 -0300

    testando email - configurando

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'smtp.sendgrid.net', port: 587}
+
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.sendgrid.net",
+    :port => 587,
+    :enable_starttls_auto => true,
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password => ENV["SENDGRID_PASSWORD"],
+    :authentication => 'login',
+    :domain => 'yourdomain.com',
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false

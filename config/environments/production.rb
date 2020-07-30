33309479ba6ea0f7a15b91c6906d8be8ecb6d16a
Lojas Pintos <danielmatos@lojaspintos.com.br>
2020-07-30 13:06:59 -0300

    configurando o envio de email - 5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    :user_name => 'daniel@pintos.com.br',
-    :password => '5=%b2qXn',
+    :user_name => 'compliance@pincred.com.br',
+    :password => 'sonilene30',
     :address => "smtp.gmail.com",
     :domain => 'smtp.gmail.com',
     :port => 587,
